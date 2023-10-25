af35867151fa40b4f0a76141e409cc962f05df04
Shabna610 <shaikshabna2023@gmail.com>
2023-10-25 18:00:52 -0500

    Made changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var mydataRouter = require('./routes/mydata');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
+app.use('/index', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
